8ddea3df7c343ae00af950c2acda3ac1fd7444a1
rrf <rubenfranco009@gmail.com>
2024-05-19 21:51:31 +0100

    Redirecionar as contas para as páginas corretas

---
+++ scripts/login.js
@@ -0,0 +1,79 @@
+"use strict";
+
+let stupid;
+
+function main() {
+  const ACTORS = {
+    "maria.fava": {
+      password: "palavrapasse",
+      role: "proprieter",
+    },
+    "luis.freitas": {
+      password: "grandecarromonstro",
+      role: "contractor",
+    },
+    "marta.residente": {
+      password: "ratosdenovaiorque",
+      role: "resident",
+    },
+  };
+
+  const LOGIN_FORM_ID = "loginForm";
+
+  const form = document.forms.namedItem(LOGIN_FORM_ID)
+  if (form === null) {
+    console.error(`Form with ID ${LOGIN_FORM_ID} not found`);
+    return;
+  }
+
+  const FORM_MESSAGE_BOX_ID = "messageBox";
+  const formMessageBox = document.getElementById(FORM_MESSAGE_BOX_ID);
+  if (formMessageBox === null) {
+    console.error(`No message box with ID ${FORM_MESSAGE_BOX_ID} found`);
+    return;
+  }
+
+  form.addEventListener(
+    "submit",
+    event => {
+      stupid = event;
+      console.log(event);
+      const name = form.elements.namedItem("usernameInput")?.value;
+      const password = form.elements.namedItem("passwordInput")?.value;
+      const fieldsValid = name in ACTORS && password === ACTORS[name].password;
+
+      if (!form.checkValidity() || !fieldsValid) {
+        event.preventDefault();
+        event.stopPropagation();
+        formMessageBox.classList.replace("opacity-0", "opacity-100");
+
+        const message = !form.checkValidity()
+          ? "Preencha ambos os campos"
+          : "Credenciais inválidas";
+        formMessageBox.innerText = message;
+      } else {
+        formMessageBox.classList.replace("opacity-100", "opacity-0");;
+
+        const role = ACTORS[name].role;
+
+        // TODO: Shouldn't there be a way to change the action URL without changing the
+        // form?
+        switch (role) {
+          case "proprieter": {
+            form.action = "/dashboard.html";
+          } break;
+          case "resident": {
+            form.action = "/report.html"
+          } break;
+          case "contractor": {
+            form.action = "/worker.html"
+          } break;
+        }
+
+        console.log(`Successfully signed in as ${role}`)
+      }
+    }
+  );
+}
+
+main();
